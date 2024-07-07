6278de2b821b44db210a8cefb4dd2519bf1831ed
minsoozz <minsoo1604@naver.com>
2024-07-07 16:45:50 +0900

    test: Number 객체 추가로 인한 테스트 수정

--- src/test/kotlin/com/github/minsoozz/CalculatorTest.kt
+++ src/test/kotlin/com/github/minsoozz/CalculatorTest.kt
@@ -2,7 +2,6 @@ package com.github.minsoozz
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 
 class CalculatorTest {
 
@@ -10,24 +9,6 @@ class CalculatorTest {
     fun `입력받은 문자열을 입력값에 따라 계산한다`() {
         val calculator = Calculator()
         val input = "2 + 3 * 2 / 5"
-        assertEquals(2, calculator.calculate(input))
-    }
-
-    @Test
-    fun `입력값이 숫자가 아닐 경우 예외를 발생한다`() {
-        val calculator = Calculator()
-        val input = "+ + 3 * 2 / 5"
-        assertThrows<IllegalArgumentException> {
-            calculator.calculate(input)
-        }
-    }
-
-    @Test
-    fun `입력값이 연산자 아닐 경우 예외를 발생한다`() {
-        val calculator = Calculator()
-        val input = "3 3 3 * 2 / 5"
-        assertThrows<IllegalArgumentException> {
-            calculator.calculate(input)
-        }
+        assertEquals(2, calculator.calculate(input).value)
     }
 }
