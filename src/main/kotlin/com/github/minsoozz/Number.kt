23a53ea8b0e0451efea010bfd54c5e5d346d4de7
minsoozz <minsoo1604@naver.com>
2024-07-07 16:28:51 +0900

    refactor: 변수명 수정

--- src/main/kotlin/com/github/minsoozz/Number.kt
+++ src/main/kotlin/com/github/minsoozz/Number.kt
@@ -1,7 +1,7 @@
 package com.github.minsoozz
 
 data class Number(val value: Int) {
-    constructor(stringValue: String) : this(
-        stringValue.toIntOrNull() ?: throw IllegalArgumentException()
+    constructor(str: String) : this(
+        str.toIntOrNull() ?: throw IllegalArgumentException()
     )
 }
